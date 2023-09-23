ebc973a98497f93127aeab8900bcc16ecac34295
faizan alam <maxchannel.mailme@gmail.com>
2023-09-23 19:44:27 +0530

    implement like n dislike functionality

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -1,10 +1,13 @@
 import {useState,useEffect} from 'react'
-import fb from '../firebase';
-const DB = fb.firestore();
-const Blogslist = DB.collection('public-blogs');
+import {updateDoc, doc, increment, getDoc} from 'firebase/firestore'
 import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
+import fb from '../firebase';
 import Trator from './Trator';
+// const currentUser = auth().currentUser;
+
+const DB = fb.firestore();
+const Blogslist = DB.collection('public-blogs');
 
 function BlogList() {
 
@@ -14,15 +17,35 @@ function BlogList() {
         const unsubscribe = Blogslist.limit(100).onSnapshot(querySnapshot =>{
             const data = querySnapshot.docs.map(doc =>({
                 ...doc.data(),
-                id: doc.id
+                id: doc.id,
+                likes:doc.data().likes,
+                dislike:doc.data().dislikes
             }));
             setBlogs(data);
         })
+        blogs.forEach(blog => {
+            const displayValue = localStorage.getItem(`likebtn-${blog.id}`);
+            if (displayValue) {
+                document.getElementById(`likebtn-${blog.id}`).style.display = displayValue;
+            }
+        });
         return unsubscribe;
-    },[])
+    },[blogs])
 
     const DeleteBlog = (id)=>{
         toast.error("Only Admin can delete the Blog") 
+    }
+
+    const handleLike = async (blogId)=>{
+        const blogRef = doc(DB, 'public-blogs',blogId);
+        await updateDoc(blogRef,{likes:increment(1)});
+        document.getElementById(`likebtn-${blogId}`).style.display = "none";
+        localStorage.setItem(`likebtn-${blogId}`, "none"); 
+    }
+    const handleDislike = async (blogId)=>{
+        const blogRef = doc(DB, 'public-blogs', blogId);
+        await updateDoc(blogRef,{dislikes:increment(1)});
+        
     }
 
   return (
@@ -36,10 +59,20 @@ function BlogList() {
                         <p className='bloguser'>user : {blog.User}</p>
                         <p className='blogbody'>{blog.Body}</p>
                         <div className="btngrp">
-                        <Link className='linkbtn' to={"/show/"+blog.id}><button>View</button></Link>
-                        <Link className='linkbtn' to={"/edit/"+blog.id}><button>Edit</button></Link>
-                        <button onClick={()=>{DeleteBlog(blog.id)}}>Delete</button>
+                            <Link className='linkbtn' to={"/show/"+blog.id}><button>View</button></Link>
+                            <Link className='linkbtn' to={"/edit/"+blog.id}> <button>Edit</button></Link>
+                            <button onClick={()=>{DeleteBlog(blog.id)}}>Delete</button>
                         </div>
+                        <div className="likedislike">
+                         <div className="likebtn">
+                            <button id={`likebtn-${blog.id}`} onClick={()=> handleLike(blog.id)}>Like</button>  <span>{blog.likes} 👍</span>
+                        </div>
+                        <div>
+                            <button className="dislikebtn" onClick={()=> handleDislike(blog.id)}>Dislike  <span>{blog.dislikes} 👎</span></button>
+                        </div>
+                        </div>
+                        
+                        
                     </div>
                 })
             }
